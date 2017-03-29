f3ec5872cc95ac8070aafacc6dcc8a6df656d66f
ankur <ankur.mcw@gmail.com>
2017-03-29 22:36:55 +0530

    Added welcome message

--- src/main/java/com/springboot/basics/BootApplication.java
+++ src/main/java/com/springboot/basics/BootApplication.java
@@ -2,13 +2,21 @@ package com.springboot.basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by renuka on 29/3/17.
  */
 
 @SpringBootApplication
+@RestController
 public class BootApplication {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
